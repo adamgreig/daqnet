b000921514b9ee3ff92c8713c8d40d063caa45ab
Adam Greig <adam@adamgreig.com>
2018-12-09 23:25:37 +0000

    Add MAC address matching to MAC

--- gateware/daqnet/ethernet/rmii.py
+++ gateware/daqnet/ethernet/rmii.py
@@ -4,6 +4,9 @@ Ethernet RMII Interface
 Copyright 2018 Adam Greig
 """
 
+from functools import reduce
+from operator import and_
+
 from migen import Module, Signal, If, Cat, FSM, NextValue, NextState
 from .crc import CRC32
 
@@ -53,6 +56,8 @@ class RMIIRx(Module):
 
         self.submodules.rxbyte = RMIIRxByte(crs_dv, rxd0, rxd1)
         self.submodules.crc = CRC32()
+        self.submodules.mac_match = MACAddressMatch(mac_addr)
+        self.submodules.fsm = FSM(reset_state="IDLE")
 
         self.comb += [
             write_port.adr.eq(self.rx_len),
@@ -60,15 +65,16 @@ class RMIIRx(Module):
             write_port.we.eq(self.rxbyte.data_valid),
             self.crc.data.eq(self.rxbyte.data),
             self.crc.data_valid.eq(self.rxbyte.data_valid),
+            self.crc.reset.eq(self.fsm.ongoing("IDLE")),
+            self.mac_match.data.eq(self.rxbyte.data),
+            self.mac_match.data_valid.eq(self.rxbyte.data_valid),
+            self.mac_match.reset.eq(self.fsm.ongoing("IDLE")),
+            self.rx_valid.eq(self.fsm.ongoing("ACK")),
         ]
-
-        self.submodules.fsm = FSM(reset_state="IDLE")
 
         # Idle until we see data valid
         self.fsm.act(
             "IDLE",
-            self.crc.reset.eq(1),
-            self.rx_valid.eq(0),
             NextValue(self.rx_len, 0),
             If(
                 self.rxbyte.dv,
@@ -79,7 +85,6 @@ class RMIIRx(Module):
         # Save incoming data to memory
         self.fsm.act(
             "DATA",
-            self.rx_valid.eq(0),
             If(
                 self.rxbyte.data_valid,
                 NextValue(self.rx_len, self.rx_len + 1)
@@ -91,10 +96,8 @@ class RMIIRx(Module):
 
         self.fsm.act(
             "EOF",
-            self.rx_valid.eq(0),
-            self.crc.data_valid.eq(0),
             If(
-                self.crc.crc_match,
+                self.crc.crc_match & self.mac_match.mac_match,
                 NextState("ACK"),
             ).Else(
                 NextState("IDLE"),
@@ -103,7 +106,6 @@ class RMIIRx(Module):
 
         self.fsm.act(
             "ACK",
-            self.rx_valid.eq(1),
             If(
                 self.rx_ack,
                 NextState("IDLE"),
@@ -127,7 +129,7 @@ class MACAddressMatch(Module):
         * `mac_match`: High if destination MAC address matches or is broadcast.
                        Remains high until `reset` is asserted.
     """
-    def __init__(self, mac_address):
+    def __init__(self, mac_addr):
         # Inputs
         self.reset = Signal()
         self.data = Signal(8)
@@ -138,46 +140,50 @@ class MACAddressMatch(Module):
 
         ###
 
-        mac = Signal(48)
-        mac_int = (
-            mac_address[0] << 40 |
-            mac_address[1] << 32 |
-            mac_address[2] << 24 |
-            mac_address[3] << 16 |
-            mac_address[4] << 8 |
-            mac_address[5])
-
-        data_reg = Signal(8)
+        mac = [Signal(8) for _ in range(6)]
 
         self.sync += [
-            self.mac_match.eq((mac == mac_int) | (mac == 0xFFFFFF)),
-            self.data_reg.eq(self.data),
+            self.mac_match.eq(
+                reduce(and_, [mac[idx] == mac_addr[idx] for idx in range(6)])
+                |
+                reduce(and_, [mac[idx] == 0xFF for idx in range(6)])
+            ),
         ]
 
         self.submodules.fsm = FSM(reset_state="RESET")
 
         self.fsm.act(
             "RESET",
-            NextValue(mac, 0),
-            NextState("IDLE"),
-        )
-
-        self.fsm.act(
-            "IDLE",
-            If(
-                self.reset == 1,
-                NextState("RESET")
-            ).Elif(
-                self.data_valid,
-                NextState("BYTE0"),
-            )
+            NextValue(mac[0], 0),
+            NextValue(mac[1], 0),
+            NextValue(mac[2], 0),
+            NextValue(mac[3], 0),
+            NextValue(mac[4], 0),
+            NextValue(mac[5], 0),
+            If(~self.reset, NextState("BYTE0")),
         )
 
         for idx in range(6):
+            next_state = f"BYTE{idx+1}" if idx < 5 else "DONE"
             self.fsm.act(
                 f"BYTE{idx}",
+                NextValue(mac[idx], self.data),
                 If(
+                    self.reset == 1,
+                    NextState("RESET")
+                ).Elif(
                     self.data_valid,
+                    NextState(next_state),
+                )
+            )
+
+        self.fsm.act(
+            "DONE",
+            If(
+                self.reset == 1,
+                NextState("RESET"),
+            )
+        )
 
 
 class RMIIRxByte(Module):
@@ -315,8 +321,9 @@ def test_rmii_rx():
 
     mem = Memory(8, 128)
     mem_port = mem.get_port(write_capable=True)
+    mac_addr = [random.randint(0, 255) for _ in range(6)]
 
-    rmii_rx = RMIIRx(mem_port, crs_dv, rxd0, rxd1)
+    rmii_rx = RMIIRx(mac_addr, mem_port, crs_dv, rxd0, rxd1)
     rmii_rx.specials += [mem, mem_port]
 
     def testbench():
@@ -441,3 +448,97 @@ def test_rmii_rx_byte():
         rmii_rx_byte = RMIIRxByte(crs_dv, rxd0, rxd1)
         run_simulation(rmii_rx_byte, testbench(rmii_rx_byte),
                        clocks={"sys": (20, 0)}, vcd_name="rmii_rx_byte.vcd")
+
+
+def test_mac_address_match():
+    import random
+    from migen.sim import run_simulation
+
+    data = Signal(8)
+    data_valid = Signal()
+    reset = Signal()
+
+    mac_address = [random.randint(0, 255) for _ in range(6)]
+    mac_address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
+    mac_matcher = MACAddressMatch(mac_address)
+
+    mac_matcher.comb += [
+        mac_matcher.data.eq(data),
+        mac_matcher.data_valid.eq(data_valid),
+        mac_matcher.reset.eq(reset),
+    ]
+
+    def testbench():
+        yield (reset.eq(1))
+        for _ in range(10):
+            yield
+        yield (reset.eq(0))
+        yield
+
+        # Check it matches its own MAC address
+        for byte in mac_address:
+            yield (data.eq(byte))
+            yield (data_valid.eq(1))
+            yield
+            yield (data_valid.eq(0))
+            yield
+
+        for idx in range(100):
+            yield (data.eq(idx))
+            yield (data_valid.eq(1))
+            yield
+            yield (data_valid.eq(0))
+            yield
+
+        assert (yield mac_matcher.mac_match) == 1
+
+        yield (reset.eq(1))
+        yield
+        yield (reset.eq(0))
+        yield
+
+        # Check it matches broadcast
+        for byte in [0xFF]*6:
+            yield (data.eq(byte))
+            yield (data_valid.eq(1))
+            yield
+            yield (data_valid.eq(0))
+            yield
+
+        for idx in range(100):
+            yield (data.eq(idx))
+            yield (data_valid.eq(1))
+            yield
+            yield (data_valid.eq(0))
+            yield
+
+        assert (yield mac_matcher.mac_match) == 1
+
+        yield (reset.eq(1))
+        yield
+        yield (reset.eq(0))
+        yield
+
+        # Check it doesn't match some other MAC address
+        for byte in mac_address[::-1]:
+            yield (data.eq(byte))
+            yield (data_valid.eq(1))
+            yield
+            yield (data_valid.eq(0))
+            yield
+
+        for idx in range(100):
+            yield (data.eq(idx))
+            yield (data_valid.eq(1))
+            yield
+            yield (data_valid.eq(0))
+            yield
+
+        assert (yield mac_matcher.mac_match) == 0
+
+        yield (reset.eq(1))
+        yield
+        yield (reset.eq(0))
+        yield
+
+    run_simulation(mac_matcher, testbench(), vcd_name="mac_matcher.vcd")
